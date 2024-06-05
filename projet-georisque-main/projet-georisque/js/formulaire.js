aac5b27960897b9026a97a5d76654da036af469c
Titianopisone <titianopisone@gmail.com>
2024-06-05 11:57:15 +0200

    form pre verif2password
    je suis en train de faire la verification des mdp entre eux

--- projet-georisque-main/projet-georisque/js/formulaire.js
+++ projet-georisque-main/projet-georisque/js/formulaire.js
@@ -1,23 +1,2 @@
-document.getElementById('myForm').addEventListener('submit', function (event){
-    event.preventDefault();
-
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    if (validateEmail(email) && validatePassword(password)) {
-
-        window.location.href = profil.html;
-    } else {
-        alert('Veuillez entrer un email et un mot de passe valides.');
-    }
-});
-
-function validateEmail(email) {
-    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
-
-function validatePassword(password) {
-    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    return re.test(password);
-}
+document.getElementById('resetButton').addEventListener('click', function() {
+    document.getElementById('myForm').reset();});
