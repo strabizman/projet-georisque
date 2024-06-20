6daf441c20b150a786c565de771142869c71319b
Pierre Six <pierrepro.six@gmail.com>
2024-06-20 10:49:43 +0200

    les checkbox fonctionne

--- projet-georisque-main/projet-georisque/js/script.js
+++ projet-georisque-main/projet-georisque/js/script.js
@@ -6,6 +6,7 @@ const UrlSeismic = 'https://www.georisques.gouv.fr/api/v1/zonage_sismique';
 const UrlReport = 'https://www.georisques.gouv.fr/api//v1/rapport_pdf';
 const LocalProxy = 'http://localhost:3000/api';
 
+// récupération des coordonées de l'adresse
 async function geocodeAddress(address) {
     const apiUrl = `${LocalProxy}/search/?q=${encodeURIComponent(address)}&limit=1`;
 
@@ -33,6 +34,7 @@ async function geocodeAddress(address) {
     }
 }
 
+// zone inondable
 async function fetchFloodZones(latitude, longitude) {
     const params = {
         latlon: `${longitude},${latitude}`,
@@ -42,7 +44,7 @@ async function fetchFloodZones(latitude, longitude) {
 
     const urlWithParams = buildUrl(UrlInondation, params);
     
-    // Ajouter cette ligne pour afficher l'URL dans la console
+    
     console.log('URL envoyée à l\'API pour les zones inondables:', urlWithParams);
 
     try {
@@ -67,6 +69,7 @@ async function fetchFloodZones(latitude, longitude) {
     }
 }
 
+// Radon
 async function fetchRadonZones(codeInsee) {
     const params = {
         code_insee: codeInsee,
@@ -97,6 +100,8 @@ async function fetchRadonZones(codeInsee) {
     }
 }
 
+// Argile
+
 async function fetchClayRisk(latitude, longitude) {
     const params = {
         latlon: `${longitude},${latitude}`
@@ -119,6 +124,8 @@ async function fetchClayRisk(latitude, longitude) {
         return null;
     }
 }
+
+// Zone Sismique
 
 async function fetchSeismicZones(rayon, latlon, codeInsee) {
     const params = {};
@@ -166,6 +173,7 @@ function buildUrl(baseUrl, params) {
     return url.toString();
 }
 
+// Compléter l'adresse automatiquement
 function setupAutocomplete() {
     const addressInput = document.getElementById('address');
 
@@ -212,6 +220,7 @@ function displayAutocompleteSuggestions(suggestions) {
     }
 }
 
+// Génération du rapport pdf
 async function generateReport(codeInsee, latlon, address) {
     const params = {
         code_insee: codeInsee,
@@ -242,110 +251,16 @@ async function generateReport(codeInsee, latlon, address) {
     }
 }
 
-document.getElementById('searchForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
-
-    const address = document.getElementById('address').value;
-    if (address) {
-        const coordinates = await geocodeAddress(address);
-        if (coordinates) {
-            console.log('Coordonnées de l\'adresse:', coordinates);
-
-            // Centrer la carte et ajouter un marqueur
-            centerMapOnCity(coordinates.latitude, coordinates.longitude);
-
-            const floodZones = await fetchFloodZones(coordinates.latitude, coordinates.longitude);
-            const radonZones = await fetchRadonZones(coordinates.codeInsee);
-            const clayRisk = await fetchClayRisk(coordinates.latitude, coordinates.longitude);
-            const seismicZones = await fetchSeismicZones(1000, `${coordinates.longitude},${coordinates.latitude}`, coordinates.codeInsee);
-
-            const resultsContainer = document.getElementById('results');
-            resultsContainer.innerHTML = '';
-
-            let cardTemplate = `
-                <div class="col-md-6 mb-3">
-                    <div class="card">
-                        <div class="card-header">{title}</div>
-                        <div class="card-body">
-                            <h5 class="card-title">{content}</h5>
-                        </div>
-                    </div>
-                </div>
-            `;
-
-            if (floodZones && floodZones.length > 0) {
-                resultsContainer.innerHTML += cardTemplate
-                    .replace('{title}', 'Zones Inondables')
-                    .replace('{content}', `L'adresse est en zone inondable avec les risques suivants : ${floodZones.join(', ')}.`);
-            } else {
-                resultsContainer.innerHTML += cardTemplate
-                    .replace('{title}', 'Zones Inondables')
-                    .replace('{content}', 'L\'adresse n\'est pas en zone inondable.');
-            }
-
-            if (radonZones && radonZones.length > 0) {
-                resultsContainer.innerHTML += cardTemplate
-                    .replace('{title}', 'Risques Radon')
-                    .replace('{content}', `L'adresse est en zone à risque radon (${radonZones[0]}).`);
-            } else {
-                resultsContainer.innerHTML += cardTemplate
-                    .replace('{title}', 'Risques Radon')
-                    .replace('{content}', 'L\'adresse n\'est pas en zone à risque radon.');
-            }
-
-            if (clayRisk) {
-                resultsContainer.innerHTML += cardTemplate
-                    .replace('{title}', 'Gonflement des Sols Argileux')
-                    .replace('{content}', `Exposition au risque de gonflement des sols argileux : ${clayRisk.exposition}.`);
-            }
-
-            if (seismicZones && seismicZones.length > 0) {
-                resultsContainer.innerHTML += cardTemplate
-                    .replace('{title}', 'Zones Sismiques')
-                    .replace('{content}', `L'adresse est en zone sismique avec un risque ${seismicZones.map(zone => ` Sismicité: ${zone.sismicite}`).join(', ')}.`);
-            } else {
-                resultsContainer.innerHTML += cardTemplate
-                    .replace('{title}', 'Zones Sismiques')
-                    .replace('{content}', 'L\'adresse n\'est pas en zone sismique.');
-            }
-
-            // Rendre le bouton de téléchargement visible
-            const downloadButton = document.getElementById('downloadReportButton');
-            downloadButton.style.display = 'block';
-
-            // Associer le téléchargement du PDF au clic sur le bouton
-            downloadButton.addEventListener('click', async function() {
-                const latlon = `${coordinates.longitude},${coordinates.latitude}`;
-                await generateReport(coordinates.codeInsee, latlon, coordinates.fullAddress);
-            });
-        } else {
-            document.getElementById('results').innerText = 'Coordonnées non trouvées.';
-        }
-    } else {
-        document.getElementById('results').innerText = 'Veuillez entrer une adresse.';
-    }
-});
-
-function centerMapOnCity(latitude, longitude) {
-    map.setView([latitude, longitude], 12); // Ajustez le niveau de zoom pour centrer sur la ville
-
-    // Ajouter un marqueur sur la carte à l'adresse recherchée
-    if (marker) {
-        map.removeLayer(marker);
-    }
-    
-}
-let map;
-let marker;
-
 document.addEventListener("DOMContentLoaded", function() {
     // Initialiser la carte centrée sur la France
     map = L.map('map').setView([46.603354, 1.888334], 6);
 
-    // Ajouter une couche de tuiles (par exemple OpenStreetMap)
+    // Ajouter une couche de tuiles de base à la carte
     const baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    });
+
+    baseLayer.addTo(map);
 
     // Ajouter des couches WMS pour les différents risques
     const floodLayer = L.tileLayer.wms('https://georisques.gouv.fr/services', {
@@ -355,27 +270,11 @@ document.addEventListener("DOMContentLoaded", function() {
         attribution: '© GéoRisques'
     });
 
-    floodLayer.on('click', function () {
-        if (map.hasLayer(floodLayer)) {
-            map.removeLayer(floodLayer);
-        } else {
-            map.addLayer(floodLayer);
-        }
-    });
-
     const radonLayer = L.tileLayer.wms('https://georisques.gouv.fr/services', {
         layers: 'RADON',
         format: 'image/png',
         transparent: true,
         attribution: '© GéoRisques'
-    });
-
-    radonLayer.on('click', function () {
-        if (map.hasLayer(radonLayer)) {
-            map.removeLayer(radonLayer);
-        } else {
-            map.addLayer(radonLayer);
-        }
     });
 
     const clayLayer = L.tileLayer.wms('https://georisques.gouv.fr/services', {
@@ -385,27 +284,11 @@ document.addEventListener("DOMContentLoaded", function() {
         attribution: '© GéoRisques'
     });
 
-    clayLayer.on('click', function () {
-        if (map.hasLayer(clayLayer)) {
-            map.removeLayer(clayLayer);
-        } else {
-            map.addLayer(clayLayer);
-        }
-    });
-
     const sismicLayer = L.tileLayer.wms('https://georisques.gouv.fr/services', {
         layers: 'SIS_INTENSITE_EVT',
         format: 'image/png',
         transparent: true,
         attribution: '© GéoRisques'
-    });
-
-    sismicLayer.on('click', function () {
-        if (map.hasLayer(sismicLayer)) {
-            map.removeLayer(sismicLayer);
-        } else {
-            map.addLayer(sismicLayer);
-        }
     });
 
     const cavityLayer = L.tileLayer.wms('https://georisques.gouv.fr/services', {
@@ -415,28 +298,12 @@ document.addEventListener("DOMContentLoaded", function() {
         attribution: '© GéoRisques'
     });
 
-    cavityLayer.on('click', function () {
-        if (map.hasLayer(cavityLayer)) {
-            map.removeLayer(cavityLayer);
-        } else {
-            map.addLayer(cavityLayer);
-        }
-    });
-
     const industryLayer = L.tileLayer.wms('https://georisques.gouv.fr/services', {
         layers: 'PPRT_COMMUNE_RISQIND_APPROUV',
         format: 'image/png',
         transparent: true,
         attribution: '© GéoRisques'
     });
-    industryLayer.on('click', function () {
-        if (map.hasLayer(industryLayer)) {
-            map.removeLayer(industryLayer);
-        } else {
-            map.addLayer(industryLayer);
-        }
-    });
-
 
     const forestFireLayer = L.tileLayer.wms('https://georisques.gouv.fr/services', {
         layers: 'SUP_FEU',
@@ -445,28 +312,11 @@ document.addEventListener("DOMContentLoaded", function() {
         attribution: '© GéoRisques'
     });
 
-    forestFireLayer.on('click', function () {
-        if (map.hasLayer(forestFireLayer)) {
-            map.removeLayer(forestFireLayer);
-        } else {
-            map.addLayer(forestFireLayer);
-        }
-    });
-
-
     const submersionLayer = L.tileLayer.wms('https://georisques.gouv.fr/services', {
         layers: 'ALEA_SYNT_03_02MOY_FXX',
         format: 'image/png',
         transparent: true,
         attribution: '© GéoRisques'
-    });
-
-    submersionLayer.on('click', function () {
-        if (map.hasLayer(submersionLayer)) {
-            map.removeLayer(submersionLayer);
-        } else {
-            map.addLayer(submersionLayer);
-        }
     });
 
     // Créer un contrôle de couches
@@ -481,32 +331,33 @@ document.addEventListener("DOMContentLoaded", function() {
         "Risque submersion fréquent": submersionLayer,
     };
 
-   // Créer un contrôle de couches avec un gestionnaire d'événements
-const controlLayers = L.control.layers(null, overlayMaps).addTo(map);
+    const controlLayers = L.control.layers(null, overlayMaps).addTo(map);
 
-let selectedLayer = null; // Variable pour suivre la couche sélectionnée
+    let selectedLayer = null; // Variable pour suivre la couche sélectionnée
 
-// Ajouter un événement 'baselayerchange' pour détecter les changements de couche
-map.on('baselayerchange', function (eventLayer) {
-    // Retirer la couche précédemment sélectionnée si elle existe
-    if (selectedLayer) {
-        map.removeLayer(selectedLayer);
-    }
+    // Ajouter des écouteurs d'événements aux cases à cocher
+    document.addEventListener('change', function(event) {
+        if (event.target.type === 'checkbox') {
+            const checkbox = event.target;
+            const layerName = checkbox.nextSibling.textContent.trim();
+            const layer = overlayMaps[layerName];
 
-    // Ajouter la nouvelle couche sélectionnée à la carte
-    selectedLayer = eventLayer.layer;
-});
-
-// Ajouter une couche de tuiles de base à la carte
-baseLayer.addTo(map);
-
-    // Ajouter un gestionnaire d'événements pour le changement de couches
-    map.on('overlayadd', function(e) {
-        updateMapTitle(e.name);
-    });
-
-    map.on('overlayremove', function(e) {
-        updateMapTitle(null);
+            if (checkbox.checked) {
+                // Ajouter la nouvelle couche et désélectionner l'ancienne si elle existe
+                if (selectedLayer && selectedLayer !== layer) {
+                    map.removeLayer(selectedLayer);
+                }
+                map.addLayer(layer);
+                selectedLayer = layer;
+                uncheckOtherCheckboxes(layerName);
+                updateMapTitle(layerName);
+            } else {
+                // Retirer la couche si elle est désélectionnée
+                map.removeLayer(layer);
+                selectedLayer = null;
+                updateMapTitle(null);
+            }
+        }
     });
 
     function updateMapTitle(layerName) {
@@ -517,7 +368,18 @@ baseLayer.addTo(map);
             mapTitle.innerText = 'Carte des risques';
         }
     }
-    
+
+    function uncheckOtherCheckboxes(selectedLayerName) {
+        // Obtenir tous les éléments de contrôle de couches
+        const overlays = document.querySelectorAll('.leaflet-control-layers-overlays input[type="checkbox"]');
+        overlays.forEach(checkbox => {
+            const label = checkbox.nextSibling.textContent.trim();
+            // Décocher toutes les autres cases sauf celle sélectionnée
+            if (label !== selectedLayerName) {
+                checkbox.checked = false;
+            }
+        });
+    }
 });
 
 window.onload = setupAutocomplete;
